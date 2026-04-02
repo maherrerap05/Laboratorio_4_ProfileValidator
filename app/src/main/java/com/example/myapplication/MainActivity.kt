d2da968af16b3a36304b214bafb76ca181b39328
Martin Herrera <maherrerap@puce.edu.ec>
2026-04-02 12:41:49 -0500

    Commit Inicial: Proyecto Validador de Perfiles (Paso 3 -4).

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,3 +17,57 @@ import androidx.compose.ui.unit.dp
 val avatarUrl: String? = null
 val userBio: String? = "Desarrollador Android en Formación."
 val followerCount: Int? = 1500
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UserProfileScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UserProfileScreen() {
+
+    // 1. Manejo seguro de nulidad con el Operador Elvis (?:)
+    val bioDisplay = userBio ?: "Biografía no Disponible."
+
+    // 2. 'when' como expresión para lógica de colores
+    val statusColor = when {
+        followerCount == null -> Color.Gray
+        followerCount > 1000 -> Color(0xFFFD700) // Dorado
+        followerCount > 100 -> Color.Blue
+        else -> Color.DarkGray
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Icono genérico con color dinámico basado en nulidad
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = null,
+            modifier = Modifier.size(100.dp),
+            tint = if(avatarUrl == null) Color.LightGray else Color.Green,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = bioDisplay, style = MaterialTheme.typography.bodyLarge)
+
+        // Llamada segura (?.) para mostrar el número de seguidores
+        Text (
+            text = "Seguidores: ${followerCount ?: 0}",
+            color = statusColor,
+            style = MaterialTheme.typography.headlineSmall,
+        )
+    }
+}
